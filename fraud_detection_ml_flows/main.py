b7a2aa15f0880e952ac47a723ca503e611ff734e
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-06-17 21:53:13 +0530

    Update main.py

--- fraud_detection_ml_flows/main.py
+++ fraud_detection_ml_flows/main.py
@@ -1,10 +1,13 @@
+import io
+import json
 from src.data_loader import load_csv_from_s3
 from src.preprocess import preprocess_data
 from src.train import train_model
 from src.logger import get_logger
 
 def main():
-    logger = get_logger("fraud_detection")
+    log_stream = io.StringIO()
+    logger = get_logger("fraud_detection", stream=log_stream)
 
     bucket_name = "lambda-code-bucket-ddd"
     object_key = "upload/transaction_samples.csv"
@@ -18,11 +21,17 @@ def main():
     logger.info("Data preprocessing complete.")
 
     train_model(X_train, X_test, y_train, y_test, scaler, logger)
-
     logger.info("Pipeline finished successfully.")
+
+    # Return logs as Lambda output
+    log_stream.seek(0)
+    return {
+        "statusCode": 200,
+        "body": log_stream.read()
+    }
 
 def lambda_handler(event, context):
     return main()
 
 if __name__ == "__main__":
-    main()
+    print(main())
