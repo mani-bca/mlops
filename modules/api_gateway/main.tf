5f5a0bc4f89dddb675da8aa2d9494f8129a6a5c5
FlyAkeed -Pratices <165019605+lavanya24072000@users.noreply.github.com>
2025-06-17 14:44:26 +0530

    Update main.tf

--- modules/api_gateway/main.tf
+++ modules/api_gateway/main.tf
@@ -1,36 +1,34 @@
-
 resource "aws_apigatewayv2_api" "api" {
-  name          = "${var.lambda_function_name}-api"
+  name          = "api_name"
   protocol_type = "HTTP"
 }
+ 
 
-resource "aws_apigatewayv2_integration" "lambda_integration" {
-  api_id           = aws_apigatewayv2_api.api.id
-  integration_type = "AWS_PROXY"
-  integration_uri  = "arn:aws:apigateway:${var.region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.region}:${data.aws_caller_identity.current.account_id}:function:${var.lambda_function_name}/invocations"
-  payload_format_version = "2.0"
+resource "aws_apigatewayv2_integration" "integration" {
+  api_id             = aws_apigatewayv2_api.api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = var.lambda_invoke_arn
   integration_method = "POST"
+  payload_format_version = "2.0"
 }
-
-
-resource "aws_apigatewayv2_route" "default_route" {
+ 
+resource "aws_apigatewayv2_route" "route" {
   api_id    = aws_apigatewayv2_api.api.id
-  route_key = "POST /"
-  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  route_key = var.route_key
+  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
 }
-
-resource "aws_apigatewayv2_stage" "default_stage" {
+ 
+resource "aws_apigatewayv2_stage" "default" {
   api_id      = aws_apigatewayv2_api.api.id
   name        = "$default"
   auto_deploy = true
 }
-
-data "aws_caller_identity" "current" {}
-
-resource "aws_lambda_permission" "apigw_invoke" {
-  statement_id  = "AllowExecutionFromAPIGateway"
+ 
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = var.lambda_function_name
+  function_name = var.lambda_arn
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
+ 
